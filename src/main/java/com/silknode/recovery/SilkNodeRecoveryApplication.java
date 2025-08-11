28d19eecaf55de080cc3b7d45afb5da5aa2837d1
edgar-partida <cesaredgar.partida@gmail.com>
2025-08-10 23:34:08 -0600

    Initial Commit

---
+++ src/main/java/com/silknode/recovery/SilkNodeRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.silknode.recovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkNodeRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilkNodeRecoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silknode/recovery/SilkNodeRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silknode.recovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilkNodeRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
